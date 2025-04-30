5c96a285a050b5b146eca3cc7b4ab66c184a8d33
Ikwanda <ikwandachairilf@gmail.com>
2025-04-30 13:55:52 +0700

    first commit

---
+++ components/LoginForm.jsx
@@ -0,0 +1,47 @@
+import IconPerson from '../src/assets/IconPerson.png'
+import IconPassword from '../src/assets/IconPassword.png'
+
+function LoginForm () {
+    return (
+        <>
+            <div className='flex justify-center items-center w-full h-screen'>
+                <div className='flex flex-col justify-center items-center bg-slate-300/70 p-6 rounded-lg backdrop-blur-md'>
+                    <img 
+                        src={IconPerson} 
+                        className='h-18 aspect-square' 
+                    />
+                    <p className='text-2xl text-slate-950'>Login Member</p>
+                    <form action="" className='flex flex-col gap-2 p-5'>
+                        <div className='flex flex-row h-12'>
+                            <input 
+                                type="text" 
+                                placeholder="email"
+                                className='relative rounded-lg px-4 bg-white w-85'
+                            />
+                            {/* <img src={IconPerson} className='absolute aspect-square object-contain h-8 w-8 mt-2 ml-1'/> */}
+                        </div>
+                        <div className='flex flex-row h-12'>
+                            <input
+                                type="text"
+                                placeholder="password"
+                                className='relative rounded-lg px-4 bg-white w-85'
+                            />
+                            {/* <img src={IconPassword} className='absolute aspect-square object-contain h-8 w-8 mt-2 ml-1'/> */}
+                        </div>
+                        <div className='flex flex-row h-12'>
+                            <button
+                                type="submit"
+                                placeholder="password"
+                                className='rounded-lg bg-slate-700 w-85 hover:bg-slate-800 hover:cursor-pointer text-sky-50'
+                            >
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LoginForm;
