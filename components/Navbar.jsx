ab64b15b1ad7864d47c4e194066b025f7864615c
Ikwanda <ikwandachairilf@gmail.com>
2025-05-01 13:32:38 +0700

    feat: update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,14 @@
 function Navbar () {
+    
     return (
         <>
-        <header className="flex flex-row justify-center gap-5 py-6 bg-slate-50">
-            <div className="bg-slate-400 px-8 py-2 rounded-4xl text-slate-50">
+        <header className="flex flex-row justify-center gap-5 py-3 bg-slate-50">
+            <div className="bg-slate-400 px-8 py-2 rounded-4xl text-slate-50 hover:cursor-pointer hover:bg-slate-500"
+                >
                 <p>Login</p>
             </div>
-            <div className="bg-slate-400 px-8 py-2 rounded-4xl text-slate-50">
+            <div className="bg-slate-400 px-8 py-2 rounded-4xl text-slate-50 hover:cursor-pointer hover:bg-slate-500"
+                >
                 <p>Daftar</p>
             </div>
         </header>
