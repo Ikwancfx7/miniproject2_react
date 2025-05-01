af27fb8163de40374b5271c2d1a55111b8b2c94a
Ikwanda <ikwandachairilf@gmail.com>
2025-05-01 09:35:22 +0700

    feat:menambah lupa password dan daftar

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -8,34 +8,40 @@ function LoginForm () {
                 <div className='flex flex-col justify-center items-center bg-slate-300/70 p-6 rounded-lg backdrop-blur-md'>
                     <img 
                         src={IconPerson} 
-                        className='h-18 aspect-square' 
-                    />
+                        className='h-18 aspect-square'/>
                     <p className='text-2xl text-slate-950'>Login Member</p>
                     <form action="" className='flex flex-col gap-2 p-5'>
                         <div className='flex flex-row h-12'>
                             <input 
                                 type="text" 
                                 placeholder="email"
-                                className='relative rounded-lg px-4 bg-white w-85'
-                            />
+                                className='rounded-lg px-4 bg-white w-85'/>
                             {/* <img src={IconPerson} className='absolute aspect-square object-contain h-8 w-8 mt-2 ml-1'/> */}
                         </div>
                         <div className='flex flex-row h-12'>
                             <input
                                 type="text"
                                 placeholder="password"
-                                className='relative rounded-lg px-4 bg-white w-85'
-                            />
+                                className='rounded-lg px-4 bg-white w-85'/>
                             {/* <img src={IconPassword} className='absolute aspect-square object-contain h-8 w-8 mt-2 ml-1'/> */}
+                        </div>
+                        <div>
+                            <a 
+                                className='text-slate-700 hover:text-slate-900'
+                                href="#"
+                            >Lupa password?</a>
                         </div>
                         <div className='flex flex-row h-12'>
                             <button
                                 type="submit"
                                 placeholder="password"
                                 className='rounded-lg bg-slate-700 w-85 hover:bg-slate-800 hover:cursor-pointer text-sky-50'
-                            >
-                                Login
-                            </button>
+                            >Login</button>
+                        </div>
+                        <div>
+                            <p>
+                                Belum punya akun? <span><a className='text-slate-700 hover:text-slate-900' href="#">Daftar</a></span>
+                            </p>
                         </div>
                     </form>
                 </div>
