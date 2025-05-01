6e04c3004a8cbf8c117285ec0ffbef2180b0be75
Ikwanda <ikwandachairilf@gmail.com>
2025-05-01 13:30:08 +0700

    feat: menambahkan page

---
+++ pages/LoginPages.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import RegisterForm from "../components/RegisterForm";
+import LoginForm from "../components/loginForm";
+
+function LoginPages () {
+    return (
+        <>
+            <Navbar/>
+            <LoginForm/>
+            <RegisterForm/>
+        </>
+    );
+};
+
+export default LoginPages;
