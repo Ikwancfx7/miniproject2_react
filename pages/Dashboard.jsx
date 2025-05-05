559d67af94e10eed5d58bb903345f4ab24ade08f
Ikwanda <ikwandachairilf@gmail.com>
2025-05-05 20:09:05 +0700

    mengganti homePages.jsx menjadi Dashboard.jsx

---
+++ pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+function Dashboard () {
+    return (
+        <>
+            <Navbar/>
+            <h1>Home</h1>
+        </> 
+    )
+}
+
+export default Dashboard;
