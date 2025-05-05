52cfe1a30191ff88f176e2a93a67fe24d810b14b
Ikwanda <ikwandachairilf@gmail.com>
2025-05-05 19:50:04 +0700

    update kode dan menghubungkan ke API resgiter

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,27 +1,76 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
 function RegisterForm () {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');  
+    const [rePassword, setRePassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        if (password !== rePassword) {
+            setMessage('Password does not match');
+            return;
+        } 
+        
+        try{
+            console.log("Email:", email);
+            console.log("Password:", password);
+
+            const response = await axios.post('https://reqres.in/api/register', {
+                email: email,
+                password: password
+            },{
+                headers: {
+                    'x-api-key': 'reqres-free-v1',
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log(response);
+            if(response.status === 200){
+                setMessage('Success');
+            }
+        } catch (error) {
+            if(error.response){
+                console.error("Error detail:", error);
+                setMessage(error.response.data.error || "Registration failed");
+            } else {
+                setMessage('Terjadi kesalahan jaringan');
+            }
+        }
+    }
+
     return (
         <>
             <div className='flex justify-center items-center w-full h-screen'>
                 <div className='flex flex-col justify-center items-center bg-slate-300/70 p-6 rounded-lg backdrop-blur-md'>
                     <p className='text-2xl text-slate-950'>Registration Form</p>
-                    <form action="" className='flex flex-col gap-2 p-5'>
+                    <form onSubmit={handleSubmit} action="" className='flex flex-col gap-2 p-5'>
                         <div className='flex flex-row h-12'>
                             <input 
                                 type="text" 
                                 placeholder="Masukkan email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 className='rounded-lg px-4 bg-white w-85'/>
                         </div>
                         <div className='flex flex-row h-12'>
                             <input
-                                type="text"
+                                type="password"
                                 placeholder="Masukkan password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 className='rounded-lg px-4 bg-white w-85'/>
                         </div>
                         <div className='flex flex-row h-12'>
                             <input
-                                type="text"
+                                type="password"
                                 placeholder="Masukkan ulang password"
+                                value={rePassword}
+                                onChange={(e) => setRePassword(e.target.value)}
                                 className='rounded-lg px-4 bg-white w-85'/>
                         </div>
                         <div className='flex flex-row h-12'>
@@ -31,6 +80,8 @@ function RegisterForm () {
                                 className='rounded-lg bg-slate-700 w-85 hover:bg-slate-800 hover:cursor-pointer text-sky-50'
                             >Daftar</button>
                         </div>
+                        {message && (
+                            <p className="text-center text-sm text-red-600 mt-2">{message}</p>)}
                         <div>
                             <p>
                                 Sudah punya akun? <span><Link 
