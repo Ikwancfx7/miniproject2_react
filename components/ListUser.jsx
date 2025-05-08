fb70777d35f8b159ee4dcfa64c1cdb12ad3292b1
Ikwanda <ikwandachairilf@gmail.com>
2025-05-08 12:00:29 +0700

    "feat: menambahkan tabel di list dan fitur next/prev page"

--- components/ListUser.jsx
+++ components/ListUser.jsx
@@ -4,37 +4,65 @@ import { Link } from "react-router-dom";
 
 function ListUser () {
     const [data, setData] = useState([]);
-    const API = 'https://reqres.in/api/users?page=2'
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const API = `https://reqres.in/api/users?page=`
 
     const getUser = async () => {
         try {
-            const response = await axios.get(API,{
+            const response = await axios.get(`${API}${page}`,{
                 headers:{
                     'x-api-key': 'reqres-free-v1',
                     'Content-Type': 'application/json'
                 }
             });
             setData(response.data.data);
+            setTotalPages(response.data.total_pages);
         } catch (error) {
             console.error(error);
         }
     }
+    const handlePageChange = () => {
+        if(page < totalPages) {
+            return setPage(page + 1);
+        }
+    }
+    const handlePrevPageChange = () => {
+        if(page > 0) {
+            return setPage(page - 1);
+        }
+    }
+
     useEffect(() => {
         getUser();
-    }, []);
+    }, [page]);
 
     return (
         <div>
             {/* <button className="bg-green-800 text-white px-3 py-2 m-2 hover:cursor-pointer" onClick={getUser}>Get User</button> */}
-            <p className="text-lg font-bold mt-10">List Users:</p>
-            <ol className="mt-2">
+            <p className="text-md lg:text-lg font-bold mt-10">List Users:</p>
+            <table className="mt-2 w-full">
+                <tr>
+                    <th className="w-12 h-12">No</th>
+                    <th>Name</th>
+                    <th>Action</th> 
+                </tr>
                 {data.map((user) => (
-                    <li className="list-decimal list-1" key={user.id}>
-                        <p>{user.first_name} {user.last_name}</p>
-                        <Link className="button-1" to={`/single-user/${user.id}`}> User Detile</Link>
-                    </li>
+                    <tr key={user.id}>
+                        <td><div className="flex items-center justify-center">{user.id}</div></td>
+                        <td><div className="flex items-center justify-center">{user.first_name} {user.last_name}</div></td>
+                        <td><Link className="button-1" to={`/single-user/${user.id}`}>Detile</Link></td>
+                    </tr>
                 ))}
-            </ol>
+            </table>
+            <div className="my-2 flex justify-end gap-3 w-full">
+                <button className="bg-green-800 text-white px-3 py-2 hover:cursor-pointer" onClick={handlePrevPageChange}>
+                    Prev Page
+                </button>
+                <button className="bg-green-800 text-white px-3 py-2 hover:cursor-pointer" onClick={handlePageChange}>
+                    Next Page
+                </button>
+            </div>
         </div>
     );
 }
--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -12,7 +12,7 @@ function Dashboard ({currentPage, setCurrentPage}) {
         <>
             <div className="w-full h-screen bg-slate-50 text-slate-950">
                 <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage('Home')}/>
-                <div className="container mx-auto">
+                <div className="container mx-auto px-10">
                     <Outlet />
                 </div>
             </div>
