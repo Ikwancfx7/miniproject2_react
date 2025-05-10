6bc6fcf1e0f83f61a474b94f5fe56c233cd9bad7
Ikwanda <ikwandachairilf@gmail.com>
2025-05-10 14:53:14 +0700

    update background

--- pages/ResgisterPages.jsx
+++ pages/ResgisterPages.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 function RegisterPages({currentPage, setCurrentPage}) {
     return (
-        <div className='w-full h-screen bg-slate-300'>
+        <div className='w-full h-screen bg-slate-100'>
             <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage('daftar')}/>
             <RegisterForm/>
         </div>
