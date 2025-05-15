c54c1ff0dfedab16d5f5f9c1ec6ebdda7bfcfa8b
Ikwanda <ikwandachairilf@gmail.com>
2025-05-15 11:54:09 +0700

    update content

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,12 +1,24 @@
 import People from '../src/assets/people.png'
+import { Link } from 'react-router-dom';
 function Landing () {
     return (
         <div className="flex flex-row justify-around items-center">
-            <p className="text-slate-950 text-[6rem] font-semibold">
-                Company <br /> Profile <br /> Website
-            </p>
+            <div className='flex justify-center flex-col gap-6'>
+                <div className='text-slate-950 text-[2rem] md:text-[5rem] font-semibold'>
+                    <p>Company</p>
+                    <p>Employee</p>
+                    <p>Profile</p>
+                </div>
+                <Link 
+                    to={'/register'}
+                    className='flex justify-center items-center py-2 w-28 md:w-44 text-sm md:text-lg rounded-sm md:rounded-lg hover:cursor-pointer text-white bg-green-900 hover:bg-green-700'
+                >
+                    <p>Register →</p>
+                </Link>
+                
+            </div>
             <div>
-                <img src={People} alt="people" className="w-[30rem]" />
+                <img src={People} alt="people" className="w-[12rem] md:w-[30rem]" />
             </div>
         </div>
     )
