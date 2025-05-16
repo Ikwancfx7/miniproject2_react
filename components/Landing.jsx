96cc45d88eb52e80cfb2e7424b0af16c092e9938
Ikwanda <ikwandachairilf@gmail.com>
2025-05-16 10:13:27 +0700

    menambah konten di mobile view

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,5 +1,11 @@
 import People from '../src/assets/people.png'
 import { Link } from 'react-router-dom';
+import Partner1 from '../src/assets/partner1.png'
+import Partner2 from '../src/assets/partner2.png'
+import Partner3 from '../src/assets/partner3.jpg'
+import Partner4 from '../src/assets/partner4.png'
+import Partner5 from '../src/assets/partner5.jpg'
+
 function Landing () {
     return (
         <div className='container mx-auto flex flex-col justify-center'>
@@ -34,6 +40,16 @@ function Landing () {
             <div className='md:hidden flex flex-col justify-center items-center mt-20'>
                 <h1 class="text-xl font-bold text-gray-800">Meet Our Team</h1>
                 <p class="text-sm text-gray-600 mt-1">Get to know the people behind our success.</p>
+            </div>
+            <div className="mt-18 md:hidden flex flex-col justify-center gap-1 items-center px-6">
+                <h1 className='text-xl font-bold text-gray-800 mb-1'>Our Partner:</h1>
+                <div className='flex flex-row justify center items-center gap-2'>
+                    <img src={Partner1} alt="Partner 1" className="partner" />
+                    <img src={Partner2} alt="Partner 2" className="partner" />
+                    <img src={Partner3} alt="Partner 3" className="partner" />
+                    <img src={Partner4} alt="Partner 4" className="partner" />
+                    <img src={Partner5} alt="Partner 5" className="partner" />
+                </div>
             </div>
         </div>
     )
